e045548a86ab1c41a2eb9d3d6482e64a9e6a8349
Bryce Gough <bryce@freeform.com.au>
2020-04-30 15:08:56 +0800

    Scroll fixes

--- assets/js/navcodes.js
+++ assets/js/navcodes.js
@@ -2,6 +2,18 @@
 
     window.navCodes = {
 
+        smoothScroll: function( target, event ) {
+            target = $( target );
+            if( target.length ) {
+                if (typeof event === 'object') {
+                    event.preventDefault();
+                }
+                $('html, body').stop().animate({
+                    scrollTop: target.offset().top - $('header').height() - 62
+                }, 1000);
+            }
+        },
+
         quicklinks: {
 
             generate: function() {
@@ -22,15 +34,16 @@
 
     };
 
-    $(document).ready(navCodes.quicklinks.generate);
-    $(document).on('click', 'a[href^="#"]', function(event) {
-        var target = $(this.getAttribute('href'));
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').stop().animate({
-                scrollTop: target.offset().top - $('header').height() - $('#wpadminbar').height() - 30
-            }, 1000);
-        }
-    });
+    // Generate Quicklinks
+    $(document).ready( navCodes.quicklinks.generate );
+
+    // Hook anchor clicks to smooth scroll
+    $(document).on('click', 'a[href^="#"]', function(e) { navCodes.smoothScroll( this.getAttribute('href'), e ); });
+
+    // Hook hashchange event to smooth scroll
+    $(window).on('hashchange', function(e) { navCodes.smoothScroll( window.location.hash, e ); });
+
+    // Hook page load with hash to smooth scroll
+    $(window).load( function(e) { if (window.location.hash.length > 0) { navCodes.smoothScroll( window.location.hash ); } });
 
 })(jQuery);
